1d80f243fe3eaa7633d351b6f2a9d01e814e5e96
Aamir041 <aamirsaudagar34@gmail.com>
2022-06-28 11:31:52 +0530

    added all

--- Hypotenuse/main.js
+++ Hypotenuse/main.js
@@ -1,10 +1,18 @@
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+
 function hypo() {
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
     var result = document.getElementById("c");
     var sqrA = Math.pow(a.value,2);
     var sqrB = Math.pow(b.value,2);
     var total = sqrA + sqrB;
-    c.value = Math.pow(total,0.5);
-    c.innerText = `${c.value}`;
+    result.value = Math.round((Math.pow(total,0.5) + Number.EPSILON) * 100) / 100; // round upto 2 decimal places
+    result.innerHTML = `Hypotenuse of <span style = "color: black;">(${a.value}<sup>2</sup>+${b.value}<sup>2</sup>)<sup>1/2</sup></span> = ${c.value}`;
+}
+
+function reset() {
+    var result = document.getElementById("c");
+    a.value = "";
+    b.value = "";
+    result.innerHTML = "";
 }
