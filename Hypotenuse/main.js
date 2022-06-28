e35e5cc24824e41d7b2ff6a63eb92a66194883d7
Aamir041 <aamirsaudagar34@gmail.com>
2022-06-28 09:18:24 +0530

    added

--- Hypotenuse/main.js
+++ Hypotenuse/main.js
@@ -1,10 +1,10 @@
 function hypo() {
     var a = document.getElementById("a");
     var b = document.getElementById("b");
-    var result = document.getElementById("result");
+    var result = document.getElementById("c");
     var sqrA = Math.pow(a.value,2);
     var sqrB = Math.pow(b.value,2);
     var total = sqrA + sqrB;
-    result.value = Math.pow(total,0.5);
-    result.innerText = `${result.value}`;
+    c.value = Math.pow(total,0.5);
+    c.innerText = `${c.value}`;
 }
